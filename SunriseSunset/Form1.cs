d14c71e5df631a2377b08290a5fa0061d6e1cee5
ziz wiz <zizwiz@engineer.com>
2024-05-15 18:56:39 +0100

    Updated the way we work out British Summer Time. The combo box is now polpulated from the XML file.

--- SunriseSunset/Form1.cs
+++ SunriseSunset/Form1.cs
@@ -2,7 +2,9 @@
 using System.Drawing;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
+using System.Xml;
 using Metar_Taf_Viewer.common_data;
 
 namespace SunriseSunset
@@ -12,13 +14,18 @@ namespace SunriseSunset
         public Form1()
         {
             InitializeComponent();
-            cmbobx_airport_info.SelectedIndex = 34;
         }
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
+            Thread myThread;
+
             //Get the data file from resources and write to file in same dir as the app.
             File.WriteAllText("airport_data.xml", Properties.Resources.airport_data);
+            //Start thread to populate the combobox after writing file above
+            //Watch cross threading
+            myThread = new Thread(new ThreadStart(PopulateAirfieldCmboBx));
+            myThread.Start();
 
             Text += " : v" + Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
 
@@ -35,9 +42,18 @@ namespace SunriseSunset
             double lng = double.Parse(data[6]);
 
 
-            int year = dateTimePicker1.Value.Year; 
+            int year = dateTimePicker1.Value.Year;
             int month = dateTimePicker1.Value.Month;
             int day = dateTimePicker1.Value.Day;
+
+            //Find last Sunday in March and October of year in datepicker
+            DateTime marchDate = CheckDate.LastSundayOfMonth("3", year.ToString());
+            DateTime octoberDate = CheckDate.LastSundayOfMonth("10", year.ToString());
+            DateTime DateToCheck = dateTimePicker1.Value;
+
+            //Are we in BritishSummerTime for chosen year?
+            bool BritishSummerTime = marchDate <= DateToCheck && DateToCheck <= octoberDate;
+
 
             ///////////////////////////////////////////////////////////////////////////////
             /// Standard Sunrise and Sunset
@@ -49,13 +65,15 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Standard Sunrise and Sunset\r");
 
             TimeSpan sunriseTime = TimeSpan.FromHours(tsunrise);
+            if (BritishSummerTime) sunriseTime = sunriseTime + new TimeSpan(1, 0, 0);
             string sunriseTimeString = sunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + sunriseTimeString + "\r");
 
             TimeSpan sunsetTime = TimeSpan.FromHours(tsunset);
+            if (BritishSummerTime) sunsetTime = sunsetTime + new TimeSpan(1, 0, 0);
             string sunsetTimeString = sunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + sunsetTimeString + "\r\r");
-            
+
             ///////////////////////////////////////////////////////////////////////////////
             /// Civil Twilight Sunrise and Sunset
             /////////////////////////////////////////////////////////////////////////////// 
@@ -65,10 +83,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Civil Twilight\r");
 
             TimeSpan CTsunriseTime = TimeSpan.FromHours(tsunrise);
+            if (BritishSummerTime) CTsunriseTime = CTsunriseTime + new TimeSpan(1, 0, 0);
             string CTsunriseTimeString = CTsunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + CTsunriseTimeString + "\r");
 
             TimeSpan CTsunsetTime = TimeSpan.FromHours(tsunset);
+            if (BritishSummerTime) CTsunsetTime = CTsunsetTime + new TimeSpan(1, 0, 0);
             string CTsunsetTimeString = CTsunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + CTsunsetTimeString + "\r\r");
 
@@ -81,10 +101,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Nautical Twilight\r");
 
             TimeSpan NTsunriseTime = TimeSpan.FromHours(tsunrise);
+            if (BritishSummerTime) NTsunriseTime = NTsunriseTime + new TimeSpan(1, 0, 0);
             string NTsunriseTimeString = NTsunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + NTsunriseTimeString + "\r");
 
             TimeSpan NTsunsetTime = TimeSpan.FromHours(tsunset);
+            if (BritishSummerTime) NTsunsetTime = NTsunsetTime + new TimeSpan(1, 0, 0);
             string NTsunsetTimeString = NTsunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + NTsunsetTimeString + "\r\r");
 
@@ -99,10 +121,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Astronomical Twilight\r");
 
             TimeSpan ATsunriseTime = TimeSpan.FromHours(tsunrise);
+            if (BritishSummerTime) ATsunriseTime = ATsunriseTime + new TimeSpan(1, 0, 0);
             string ATsunriseTimeString = ATsunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + ATsunriseTimeString + "\r");
 
             TimeSpan ATsunsetTime = TimeSpan.FromHours(tsunset);
+            if (BritishSummerTime) ATsunsetTime = ATsunsetTime + new TimeSpan(1, 0, 0);
             string ATsunsetTimeString = ATsunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + ATsunsetTimeString + "\r\r");
         }
@@ -115,6 +139,39 @@ namespace SunriseSunset
         private void btn_reset_Click(object sender, EventArgs e)
         {
             rchtxbx_output.Text = "";
+        }
+
+        void PopulateAirfieldCmboBx()
+        {
+            // A different thread watch cross threading
+            //populate the combo boxes with the airfield names direct from xml file so we get 
+            //names correctly spelt for later look up
+            XmlDocument doc = new XmlDocument();
+            doc.Load("airport_data.xml");
+            XmlNodeList airportList = doc.SelectNodes("uk_airports/airport_info/airport_name");
+            foreach (XmlNode Name in airportList)
+            {
+                if (Name.InnerText != "UN-ASSIGNED") // if there is am actual airport
+                {
+                    string[] data = airport_data.GetAirportInfo(Name.InnerText);
+
+                    if (data[8] != "") // only include if we have data for airport in this case altitude in metres
+                    {
+                        //use invoke ot prevent cross threading
+                        BeginInvoke(new Action(() =>
+                        {
+                            cmbobx_airport_info.Items.Add(Name.InnerText);
+                        }));
+                    }
+                }
+            }
+
+            //use invoke ot prevent cross threading
+            //Set to Cambridge City Airport
+            BeginInvoke(new Action(() =>
+            {
+                cmbobx_airport_info.SelectedIndex = 33;
+            }));
         }
     }
 }
